9b1077a029bd632df86fb263c73b572076dd7806
Morgan <morgan@ischool.berkeley.edu>
2013-10-05 22:06:54 -0700

    Bootstrap integration; Form validation;short url randomization

--- server/app.py
+++ server/app.py
@@ -16,18 +16,20 @@ db = shelve.open("shorten.db")
 # Home Resource:
 # Only supports the GET method, returns a homepage represented as HTML
 ###
+@app.route('/')
 @app.route('/home', methods=['GET'])
 def home():
     """Builds a template based on a GET request, with some default
     arguements"""
-    index_title = request.args.get("title", "URL Shortener")
+    index_title = request.args.get("title", "i253")
+    hello_name = request.args.get("name", "Jim")
     return flask.render_template(
             'home.html',
-            title=index_title)
+            title=index_title,
+            name=hello_name)
 
 
-###
-# Wiki Resource:
+
 # GET method will redirect to the short-url stored in db
 # POST/PUT method will update the redirect destination
 ###
@@ -45,6 +47,7 @@ def error():
     
 @app.errorhandler(404)
 def page_not_found(e):
+	"""Handles all requests that the server can't handle"""
 	return flask.render_template("404.html",page=e)
 
 
@@ -58,35 +61,6 @@ def shorten_url():
     db[short_url] = long_url
     return "associated " + long_url + " with  " + short_url
 
-###
-# i253 Resource:
-# Information on the i253 class. Can be parameterized with `relationship`,
-# `name`, and `adjective` information
-#
-# TODO: The representation for this resource is broken. Fix it!
-# Set the correct MIME type to be able to view the image in your browser
-##/
-@app.route('/i253')
-def i253():
-    """Returns a PNG image of madlibs text"""
-    relationship = request.args.get("relationship", "friend")
-    name = request.args.get("name", "Jim")
-    adjective = request.args.get("adjective", "fun")
-
-    resp = flask.make_response(
-            check_output(['convert', '-size', '600x400', 'xc:transparent',
-                '-frame', '10x30',
-                '-font', '/usr/share/fonts/liberation/LiberationSerif-BoldItalic.ttf',
-                '-fill', 'black',
-                '-pointsize', '32',
-                '-draw',
-                  "text 30,60 'My %s %s said i253 was %s'" % (relationship, name, adjective),
-                '-raise', '30',
-                'png:-']), 200);
-    # Comment in to set header below
-    # resp.headers['Content-Type'] = '...'
-
-    return resp
 
 
 if __name__ == "__main__":
--- server/static/js/main.js
+++ server/static/js/main.js
@@ -1,15 +1,18 @@
 function validate() {
 
         var letters = /^[A-Za-z]+$/;  
-        var shortUrl = $("input[type='text'][name='short-url']").val();
+        var longurl = $("input[name='long-url']").val();
+        
             
-        if(shortUrl.length === 0){
+        if(longurl.length === 0){
             //error
+/*             alert("la") */
             console.log("please enter something (letters only) for the short url");
+            $("#long-url").parent().toggleClass('has-error');
             return false;
 	}
         else {
-            if(shortUrl.match(letters)) {
+            if(longurl.match(letters)) {
                 //all good
             }
             else{
@@ -20,7 +23,28 @@ function validate() {
         }
 }
 
+//automatically create a shortened version of the url
+function makeshort(){
+/* 	var pathname = window.location.pathname; */
+	shorturl_rand=makeid()
+	$("input[name='short-url']").val(shorturl_rand)
+	return shorturl_rand
+	
+}
+
+function makeid()
+{
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
 $(document).ready(function() {
         $("#url-shorten").submit(validate);
+        $("#short-url").val(makeshort)
         console.log("ready!");
 });
